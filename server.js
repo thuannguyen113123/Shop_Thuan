c217fa70d09fef2af5a70873eb685e5dbbedf392
thuannguyen113123 <91785118+thuannguyen113123@users.noreply.github.com>
2024-03-04 14:22:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,18 +6,20 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import morgan from "morgan";
-import path from "path";
 import { fileURLToPath } from "url";
+import path from "path";
 
-//Cấu hình env(môi trường)
+// Cấu hình env(môi trường)
 dotenv.config();
 
+// Lấy đường dẫn tới file đang thực thi
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename); // Di chuyển đây
+// Xác định __dirname từ __filename
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// //Kết nối cơ sở dữ liệu
+// Kết nối cơ sở dữ liệu
 connectDB();
 
 app.use(cors());
@@ -25,15 +27,14 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-
-//đường dẫn
-//xứ lý đăng nhập
+// Đường dẫn
+// Xử lý đăng nhập
 app.use("/api/v1/auth", authRoutes);
-// danh mục
+// Danh mục
 app.use("/api/v1/category", categoryRoutes);
-//sản phẩm
+// Sản phẩm
 app.use("/api/v1/product", productRoutes);
-//rest api
+// REST API
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to ecommerce app</h1>");
 });
@@ -41,7 +42,7 @@ app.get("/", (req, res) => {
 app.use("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-//Port
+// Port
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
